9f79e87bd22167b2e1c5a7a85e768cdd15756f10
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2023-10-18 17:38:29 +0200

    exclude CRS that are not supported by proj.js

--- makeCSV.js
+++ makeCSV.js
@@ -1,20 +1,41 @@
-const all = require('epsg-index/all.json')
+const all = require("epsg-index/all.json")
+const proj = require("proj4");
+
+let count = 0;
 
 for (let crs of Object.values(all)) {
   //console.log(crs.kind + ":" + crs.name);
   // only use projected or geographic CRS, ignore planetary CRS
   if (["CRS-PROJCRS","CRS-GEOGCRS"].includes(crs.kind) && crs.code<104872 && !(crs.code >= 103877 && crs.code <= 103887 )) {
-    let bounds = crs.bbox;
-    let boundsStr = "-";
     
-    if (bounds[0]<90 || bounds[1]>-180 || bounds[2]>-90 || bounds[3]<180) {
-      boundsStr = bounds.join(";")
+    let valid = false;
+    
+    try {
+      let p = proj(crs.proj4);
+      valid = true;
+    }
+    catch (e) {
+      // ignore errors, but don't generate output line for unsupported CRS
+      //console.error("Error with " + crs.code);
+      //console.error(e);
     }
     
-    let kind = crs.kind == "CRS-PROJCRS" ? "P":"G";
-    
-    let name = crs.name.replace(/\,/g,";");
-    
-    console.log([crs.code,kind,name,boundsStr].join(","));
+    if (valid) {
+      let bounds = crs.bbox;
+      let boundsStr = "-";
+      
+      if (bounds[0]<90 || bounds[1]>-180 || bounds[2]>-90 || bounds[3]<180) {
+        boundsStr = bounds.join(";")
+      }
+      
+      let kind = crs.kind == "CRS-PROJCRS" ? "P":"G";
+      
+      let name = crs.name.replace(/\,/g,";");
+      
+      console.log([crs.code,kind,name,boundsStr].join(","));
+      count++;
+    }
   }
 }
+
+console.error(count + " entries generated.");
